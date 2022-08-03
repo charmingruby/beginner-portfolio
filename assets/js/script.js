3366cd664e1361aafd6fca9daf6e175d9be6b7b1
charmingruby <mrrubycanal3105@gmail.com>
2022-08-03 13:44:10 -0300

    good progress in skills section

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,24 @@ window.addEventListener('scroll', () => {
     scrolled = actualScroll;    
 });
 
+const accordionHeaders = document.querySelectorAll('.accordion__header');
+
+accordionHeaders.forEach(accordionHeader => {
+    accordionHeader.addEventListener('click', () => {
+        const height = accordionHeader.nextElementSibling.scrollHeight
+        const accordionBtn = accordionHeader.querySelector('.accordion__button');
+        accordionHeader.classList.add('active');
+        if(accordionHeader.classList.contains('active')) {
+            accordionHeaders.forEach(item => {
+                item.nextElementSibling.style.maxHeight = '0px';
+                const itemBtn = item.querySelector('.accordion__button');
+                itemBtn.innerHTML = '+';
+            })
+            accordionHeader.nextElementSibling.style.maxHeight = `${height}px`
+            accordionBtn.innerHTML = '-';
+        } else {
+            accordionHeader.nextElementSibling.style.maxHeight = '0px';
+            accordionBtn.innerHTML = '+';
+        }
+    })
+})
