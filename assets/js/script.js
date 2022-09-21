c66d7ed23d8d1140b2947b2923b7236ccfcc33d8
charmingruby <mrrubycanal3105@gmail.com>
2022-09-21 14:27:52 -0300

    eliminated css vars to fit in samsung browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,29 +34,6 @@ const navCheck = (res) => {
     }
 }
 
-const fnBrowserDetect = () => {
-    let userAgent = navigator.userAgent;
-    let browserName;
-
-    if (userAgent.match(/chrome|chromium|crios/i)) {
-        browserName = "chrome";
-    } else if (userAgent.match(/firefox|fxios/i)) {
-        browserName = "firefox";
-    }  else if (userAgent.match(/safari/i)) {
-        browserName = "safari";
-    } else if (userAgent.match(/opr/i)) {
-        browserName = "opera";
-    } else if (userAgent.match(/edg/i)) {
-        browserName = "edge";
-    } else {
-        window.location.replace('../../error.html');
-        browserName="No browser detection";
-    }
-    
-}
-
-fnBrowserDetect();
-
 /* E V E N T S */
 /// Menu Toggle
 menu.addEventListener('click', (e) => {
