9d92c37123d2ee9aa1b58536a63f4cee359c8718
charmingruby <mrrubycanal3105@gmail.com>
2022-09-21 11:38:52 -0300

    fix browsers

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,27 @@ const navCheck = (res) => {
     }
 }
 
+
+const fnBrowserDetect = () => {
+    let userAgent = navigator.userAgent;
+    let browserName;
+
+    if (userAgent.match(/chrome|chromium|crios/i)) {
+        browserName = "chrome";
+    } else if (userAgent.match(/firefox|fxios/i)) {
+        browserName = "firefox";
+    }  else if (userAgent.match(/safari/i)) {
+        browserName = "safari";
+    } else if (userAgent.match(/opr/i)) {
+        browserName = "opera";
+    } else if (userAgent.match(/edg/i)) {
+        browserName = "edge";
+    } else {
+        browserName="No browser detection";
+    }
+     document.querySelector("h1").innerText="You are using "+ browserName +" browser";
+}
+
 /* E V E N T S */
 /// Menu Toggle
 menu.addEventListener('click', (e) => {
